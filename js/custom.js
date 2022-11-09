4f217cf28aa4e22d3144ede8b2111908ea4c7a56
yslee1126 <yslee1126@gmail.com>
2022-11-09 16:26:59 +0900

    category finish

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,12 @@
 $(function(){
     $('.gnb-include').load('/include/gnb.html')
     $('.main-header-include').load('/include/main-header.html')
+
+    $('.category-accordion .title').click(function(){
+        $(this).next().stop().slideDown();
+        $(this).siblings('.category-accordion .title').next().stop().slideUp();
+        $(this).addClass('active')
+        $(this).siblings('.category-accordion .title').removeClass('active')
+    })
+
 })
